5226d8fb298762324e3c6b2c8a8e2eeb1d60e953
Ahmed Salem <ahmedsalem2002m@gmail.com>
2026-07-06 19:23:41 +0300

    Edit container

--- lib/main.dart
+++ lib/main.dart
@@ -40,34 +40,74 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold
                 ),
               ),
-              Divider(
-
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12)
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                margin: EdgeInsets.symmetric(horizontal: 24),
-
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.phone_rounded,
-                      size: 32,
-                      color: Color(0xff2B475E),
-                    ),
-                    Text(
-                      "(+02) 01030759478",
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0,vertical: 6),
+                child: Container(
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8)
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 12.0),
+                        child: Icon(
+                          Icons.phone_rounded,
+                          size: 32,
+                          color: Color(0xff2B475E),
+                        ),
                       ),
-                    )
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.only(left: 24.0),
+                        child: Text(
+                          "(+02) 01030759478",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  ),
                 ),
-              )
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0,vertical: 6),
+                child: Container(
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8)
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 12.0),
+                        child: Icon(
+                          Icons.mail,
+                          size: 32,
+                          color: Color(0xff2B475E),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 24.0),
+                        child: Text(
+                          "ahmedsalem@gmail.com",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ),
+              ),
+
+
             ],
           ),
         ),
